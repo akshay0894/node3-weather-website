36439b01a4a458becdb74579d6619a09f70afbc2
akshay0894 <akshay.mittal0894@gmail.com>
2019-04-06 14:17:02 +0530

    preparing for heroku

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const path=require('path');
 const hbs= require('hbs');
 const geocode=require('./utils/geocode');
 const forecast=require('./utils/forecast');
+const port=process.env.PORT || 3000;
 const publicDirectoryPath=path.join(__dirname,'../public');
 app.use(express.static(publicDirectoryPath));
 
@@ -74,6 +75,6 @@ app.get('*',(req,res)=>
     });
 })
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log("sever is listening");
 })
